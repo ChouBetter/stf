0b2ed244b6a9eadec316df9a2923bba14cc84d41
Better <>
2019-09-29 00:08:49 +0800

    test

--- res/app/components/stf/device/device-service.js
+++ res/app/components/stf/device/device-service.js
@@ -1,52 +1,54 @@
-var oboe = require('oboe')
-var _ = require('lodash')
-var EventEmitter = require('eventemitter3')
+var oboe = require("oboe");
+var _ = require("lodash");
+var EventEmitter = require("eventemitter3");
 
-module.exports = function DeviceServiceFactory($http, socket, EnhanceDeviceService) {
-  var deviceService = {}
+module.exports = function DeviceServiceFactory(
+  $http,
+  socket,
+  EnhanceDeviceService
+) {
+  var deviceService = {};
 
   function Tracker($scope, options) {
-    var devices = []
-    var devicesBySerial = Object.create(null)
-    var scopedSocket = socket.scoped($scope)
-    var digestTimer, lastDigest
+    var devices = [];
+    var devicesBySerial = Object.create(null);
+    var scopedSocket = socket.scoped($scope);
+    var digestTimer, lastDigest;
 
-    $scope.$on('$destroy', function() {
-      clearTimeout(digestTimer)
-    })
+    $scope.$on("$destroy", function() {
+      clearTimeout(digestTimer);
+    });
 
     function digest() {
       // Not great. Consider something else
       if (!$scope.$$phase) {
-        $scope.$digest()
+        $scope.$digest();
       }
 
-      lastDigest = Date.now()
-      digestTimer = null
+      lastDigest = Date.now();
+      digestTimer = null;
     }
 
     function notify(event) {
       if (!options.digest) {
-        return
+        return;
       }
 
       if (event.important) {
         // Handle important updates immediately.
         //digest()
-        window.requestAnimationFrame(digest)
-      }
-      else {
+        window.requestAnimationFrame(digest);
+      } else {
         if (!digestTimer) {
-          var delta = Date.now() - lastDigest
+          var delta = Date.now() - lastDigest;
           if (delta > 1000) {
             // It's been a while since the last update, so let's just update
             // right now even though it's low priority.
-            digest()
-          }
-          else {
+            digest();
+          } else {
             // It hasn't been long since the last update. Let's wait for a
             // while so that the UI doesn't get stressed out.
-            digestTimer = setTimeout(digest, delta)
+            digestTimer = setTimeout(digest, delta);
           }
         }
       }
@@ -56,167 +58,166 @@ module.exports = function DeviceServiceFactory($http, socket, EnhanceDeviceServi
       // usable IF device is physically present AND device is online AND
       // preparations are ready AND the device has no owner or we are the
       // owner
-      data.usable = data.present && data.status === 3 && data.ready &&
-        (!data.owner || data.using)
+      data.usable =
+        data.present &&
+        data.status === 3 &&
+        data.ready &&
+        (!data.owner || data.using);
 
       // Make sure we don't mistakenly think we still have the device
       if (!data.usable || !data.owner) {
-        data.using = false
+        data.using = false;
       }
 
-      EnhanceDeviceService.enhance(data)
+      EnhanceDeviceService.enhance(data);
     }
 
     function get(data) {
-      return devices[devicesBySerial[data.serial]]
+      return devices[devicesBySerial[data.serial]];
     }
 
     var insert = function insert(data) {
-      devicesBySerial[data.serial] = devices.push(data) - 1
-      sync(data)
-      this.emit('add', data)
-    }.bind(this)
+      devicesBySerial[data.serial] = devices.push(data) - 1;
+      sync(data);
+      this.emit("add", data);
+    }.bind(this);
 
     var modify = function modify(data, newData) {
       _.merge(data, newData, function(a, b) {
         // New Arrays overwrite old Arrays
         if (_.isArray(b)) {
-          return b
+          return b;
         }
-      })
-      sync(data)
-      this.emit('change', data)
-    }.bind(this)
+      });
+      sync(data);
+      this.emit("change", data);
+    }.bind(this);
 
     var remove = function remove(data) {
-      var index = devicesBySerial[data.serial]
+      var index = devicesBySerial[data.serial];
       if (index >= 0) {
-        devices.splice(index, 1)
-        delete devicesBySerial[data.serial]
-        this.emit('remove', data)
+        devices.splice(index, 1);
+        delete devicesBySerial[data.serial];
+        this.emit("remove", data);
       }
-    }.bind(this)
+    }.bind(this);
 
     function fetch(data) {
-      deviceService.load(data.serial)
+      deviceService
+        .load(data.serial)
         .then(function(device) {
           return changeListener({
-            important: true
-          , data: device
-          })
+            important: true,
+            data: device
+          });
         })
-        .catch(function() {})
+        .catch(function() {});
     }
 
     function addListener(event) {
-      var device = get(event.data)
+      var device = get(event.data);
       if (device) {
-        modify(device, event.data)
-        notify(event)
-      }
-      else {
+        modify(device, event.data);
+        notify(event);
+      } else {
         if (options.filter(event.data)) {
-          insert(event.data)
-          notify(event)
+          insert(event.data);
+          notify(event);
         }
       }
     }
 
     function changeListener(event) {
-      var device = get(event.data)
+      var device = get(event.data);
       if (device) {
-        modify(device, event.data)
+        modify(device, event.data);
         if (!options.filter(device)) {
-          remove(device)
+          remove(device);
         }
-        notify(event)
-      }
-      else {
+        notify(event);
+      } else {
         if (options.filter(event.data)) {
-          insert(event.data)
+          insert(event.data);
           // We've only got partial data
-          fetch(event.data)
-          notify(event)
+          fetch(event.data);
+          notify(event);
         }
       }
     }
 
-    scopedSocket.on('device.add', addListener)
-    scopedSocket.on('device.remove', changeListener)
-    scopedSocket.on('device.change', changeListener)
+    scopedSocket.on("device.add", addListener);
+    scopedSocket.on("device.remove", changeListener);
+    scopedSocket.on("device.change", changeListener);
 
     this.add = function(device) {
       addListener({
-        important: true
-      , data: device
-      })
-    }
+        important: true,
+        data: device
+      });
+    };
 
-    this.devices = devices
+    this.devices = devices;
   }
 
-  Tracker.prototype = new EventEmitter()
+  Tracker.prototype = new EventEmitter();
 
   deviceService.trackAll = function($scope) {
     var tracker = new Tracker($scope, {
       filter: function() {
-        return true
-      }
-    , digest: false
-    })
+        return true;
+      },
+      digest: false
+    });
 
-    oboe('/api/v1/devices')
-      .node('devices[*]', function(device) {
-        tracker.add(device)
-      })
+    oboe("/api/v1/devices").node("devices[*]", function(device) {
+      // tracker.add(device)
+      console.log(device);
+    });
 
-    return tracker
-  }
+    return tracker;
+  };
 
   deviceService.trackGroup = function($scope) {
     var tracker = new Tracker($scope, {
       filter: function(device) {
-        return device.using
-      }
-    , digest: true
-    })
+        return device.using;
+      },
+      digest: true
+    });
 
-    oboe('/api/v1/user/devices')
-      .node('devices[*]', function(device) {
-        tracker.add(device)
-      })
+    oboe("/api/v1/user/devices").node("devices[*]", function(device) {
+      tracker.add(device);
+    });
 
-    return tracker
-  }
+    return tracker;
+  };
 
   deviceService.load = function(serial) {
-    return $http.get('/api/v1/devices/' + serial)
-      .then(function(response) {
-        return response.data.device
-      })
-  }
+    return $http.get("/api/v1/devices/" + serial).then(function(response) {
+      return response.data.device;
+    });
+  };
 
   deviceService.get = function(serial, $scope) {
     var tracker = new Tracker($scope, {
       filter: function(device) {
-        return device.serial === serial
-      }
-    , digest: true
-    })
+        return device.serial === serial;
+      },
+      digest: true
+    });
 
-    return deviceService.load(serial)
-      .then(function(device) {
-        tracker.add(device)
-        return device
-      })
-  }
+    return deviceService.load(serial).then(function(device) {
+      tracker.add(device);
+      return device;
+    });
+  };
 
   deviceService.updateNote = function(serial, note) {
-    socket.emit('device.note', {
+    socket.emit("device.note", {
       serial: serial,
       note: note
-    })
-  }
+    });
+  };
 
-  return deviceService
-}
+  return deviceService;
+};
