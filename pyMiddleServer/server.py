ed83f2e3638b21043fbd29dd645d2b5e75675aa5
Better <>
2019-12-15 16:43:03 +0800

    change script serial mapping

--- pyMiddleServer/server.py
+++ pyMiddleServer/server.py
@@ -5,7 +5,8 @@ import json
 import commentjson
 import os
 
-def shutdown(): 
+
+def shutdown():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         return 'You can close this window and terminate the script.'
@@ -13,19 +14,25 @@ def shutdown():
         func()
         return 'Server is shutting down, so you can close this window.'
 
+
 app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def index():
     return "success"
 
-@app.route('/auto/<serial>/<script>')
-def setting(serial, script):
-	# print os.popen('docker exec -it appium1 adb connect ' + serial).read().strip()
-	# print '---------------------------------------------------------------'
-	print os.popen('python script/' + script + ' ' + serial).read().strip()
-	# print '---------------------------------------------------------------'
-	# print os.popen('docker exec -it appium1 adb disconnect ' + serial).read().strip()
-	return serial + ' ' + script
 
-app.run(host='0.0.0.0',port=3001)
+@app.route('/auto/<script>/<serial>')
+def _auto(script, serial):
+    print os.popen('python script/' + script + ' ' + serial).read().strip()
+    return serial + ' ' + script
+
+
+@app.route('/shell/<script>/<serial>')
+def _auto(script, serial):
+    print os.popen('./shell/' + script + ' ' + serial).read().strip()
+    return serial + ' ' + script
+
+
+app.run(host='0.0.0.0', port=3001)
