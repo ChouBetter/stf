fbc82402f5f0c3a0e78ca8140d19fbc31cbf357b
Better <>
2020-04-14 13:04:26 +0800

    no message

--- pyMiddleLDAP/server.py
+++ pyMiddleLDAP/server.py
@@ -14,6 +14,8 @@ LDAP_BIND = "cn=admin,dc=androidcloud,dc=cn"
 LDAP_BIND_PASS = "^andr0id2019$"
 LDAP_ADD_DN = ",dc=androidcloud,dc=cn"
 
+LDAP_BASE_DN = "dc=androidcloud,dc=cn"
+
 app = Flask(__name__, static_url_path='/static')
 # CORS(app)
 
@@ -52,6 +54,8 @@ def _add_o(account, password, owner):
     attrs["sn"] = account
     attrs["userPassword"] = password
     attrs['description'] = 'ini group untuk semua dosen dokter'
+    if owner != '':
+        attrs['o'] = owner.encode('utf-8')
 
     ldif = modlist.addModlist(attrs)
     print("modlist.addModlist")
@@ -59,6 +63,50 @@ def _add_o(account, password, owner):
     try:
         result = l.add_s(dn, ldif)
         print("l.add_s")
+    except Exception as e:
+        l.unbind_s()
+        print("l.unbind_s")
+        return str(e)
+
+    l.unbind_s()
+    print("l.unbind_s")
+
+    return str(result)
+
+
+@app.route('/sub/<account>')
+def _sub(account):
+    l = ldap.initialize(LDAP_SERVER)
+    print("ldap.initialize")
+    print(l.simple_bind_s(LDAP_BIND, LDAP_BIND_PASS))
+    print("l.simple_bind_s")
+
+    try:
+        result = l.search_s(LDAP_BASE_DN, ldap.SCOPE_SUBTREE,
+                            "o="+account.encode('utf-8'), None)
+        print("l.search_s")
+    except Exception as e:
+        l.unbind_s()
+        print("l.unbind_s")
+        return str(e)
+
+    l.unbind_s()
+    print("l.unbind_s")
+
+    return str(result)
+
+
+@app.route('/search/<account>')
+def _search(account):
+    l = ldap.initialize(LDAP_SERVER)
+    print("ldap.initialize")
+    print(l.simple_bind_s(LDAP_BIND, LDAP_BIND_PASS))
+    print("l.simple_bind_s")
+
+    try:
+        result = l.search_s(LDAP_BASE_DN, ldap.SCOPE_SUBTREE,
+                            "cn="+account.encode('utf-8'), None)
+        print("l.search_s")
     except Exception as e:
         l.unbind_s()
         print("l.unbind_s")
